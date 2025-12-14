bc1a159cda3e08ee2e05e77b1dfcaf81dd2070da
jcarlo0118 <jhames.carlo718@gmail.com>
2025-12-14 22:06:40 +0100

    rough implementation of combat system

--- src/gameController/GameState.java
+++ src/gameController/GameState.java
@@ -1,10 +1,70 @@
 package gameController;
 /*
  * stato del gioco:
- * e.g se il giocatore sta combattendo oppure se sta nel menu
+ * Scenario: the player encounters an enemy in the same room,
+ * 1. a battle loop will be initiated
+ * 1a. the game state gets saved
+ * 
+ * 2. after the battle is over
+ * 2a. the player loses: gameover the game gets terminated
+ * 2b. the enemy loses: it gets removed from the labyrinth and the arraylist that contains the monster and their positions
+ * 
+ * 3. the game state loads
  * 
  * 
  */
-public class GameState {
 
+import java.util.ArrayList;
+import java.util.List;
+
+import labyrinth.*;
+import labyrinth.entities.*;
+
+public class GameState {
+	public Player player;
+	public Room[][] grid;
+	public Enemy enemy;
+	//public List<Enemy> enemy;
+	
+	public GameState(Player player, Room[][] grid) {
+		this.player = player;
+		this.grid = grid;
+		//this.enemy = new ArrayList<>();
+	}
+	
+	/*public Enemy getEnemyAt(Position pos) {
+		for (Enemy e: enemy) {
+			if (e.isAlive() && e.getPosition().equals(pos)) {
+				return e;
+			}
+		}
+		return null;
+	}*/
+	
+	//========================TEMPORARY, FOR TESTING=====================================//
+	public Enemy getSingleEnemy() {
+		return this.enemy;
+	}
+	
+	public void setSingleEnemy(Enemy enemy) {
+		this.enemy = enemy;
+	}
+	//========================TEMPORARY, FOR TESTING=====================================//
+	
+	public Player getPlayer() {
+		return this.player;
+	}
+	
+	public void setPlayer(Player player) {
+		this.player = player;
+	}
+	
+	public GameState load() {// to be implemented
+		return null;
+	}
+	
+	public GameState save(Player player, Room[][] grid, List<Enemy> enemy) {// to be implemented
+		return null;
+	}
+	
 }
--- src/labyrinth/Labyrinth.java
+++ src/labyrinth/Labyrinth.java
@@ -1,23 +1,58 @@
 package labyrinth;
+//import labyrinth.entities.*;
+
+import java.util.ArrayList;
+
 import labyrinth.entities.*;
 
 public class Labyrinth {
-	private int height, width;
-	private int[][] grid;
+	private int rows, columns;
+	private Room[][] grid;
+	private ArrayList<Enemy> enemies;
 	
+	private final Room start;//
+	private final Room end;//
 	
-	
-	
-	public int[][] getGrid() {
-		return this.grid;
+	public Labyrinth(int rows, int columns, Room start, Room end) {
+		this.rows = rows <= 0 ? 1 : rows;
+		this.columns = columns <= 0 ? 1 : columns;
+		this.start = start;
+		this.end = end;
+		//this.end.setPosition(new Position(rows - 1, columns - 1));
+		
 	}
-	public void setGrid(int height, int width) {
-		int[][] rooms = new int[height][width];	
+	
+	public Room[][] createRooms() {
+		Room[][] grid = new Room[this.rows][this.columns];
+		int x,y;
+		for (x = 0; x < grid.length; x++) {
+			for (y = 0; y < grid[x].length; y++) {
+					grid[x][y] = new Room(new Position(x,y));
+			}	
+		}
+		return grid;			
 	}
-	public int getHeight() {return height;}
-	public void setHeight(int height) {this.height = height;}
-	public int getWidth() {return width;}
-	public void setWidth(int width) {this.width = width;}
 	
+	//generate enemies
 	
+	//place enemies
+	
+	//create enemies
+	
+	//place player
+	
+	//get enemies so they can be saved in game state
+	
+	//get player  so they can be saved in game state
+
+	public int getRows() {return rows;}
+	public void setRows(int rows) {this.rows = rows;}
+	public int getColumns() {return columns;}
+	public void setColumns(int columns) {this.columns = columns;}
+	
+	public Room getStart() {return start;}
+	public Room getEnd() {return end;}
+	
+	public Room[][] getGrid() {return this.grid;}
+	public void setGrid(Room[][] newGrid) {this.grid = newGrid;}
 }
